93de3ff5abbacd4a23935d61018cee07ae9bfe35
Mike Tallerico <mtallerico1@gmail.com>
2018-06-26 19:41:26 -0400

    scraping data

--- scraper.js
+++ scraper.js
@@ -1,5 +1,36 @@
-const scraper = require('scrape-it');
+const scrapeIt = require('scrape-it');
 const fs = require('fs');
-const csvParse = require('json2csv').Parser;
 const folderExist = fs.existsSync('./data');
+const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+const opts = { fields };
 
+scrapeIt('http://shirts4mike.com/shirts.php', {
+    tshirts: {
+        listItem: ".products li"
+        , name: "a"
+        , data: {
+             url: {
+                  selector: "a"
+                , attr: "href"
+              },
+              img: {
+                  selector: "img",
+                  attr: "src"
+              }
+          }
+      }
+    
+}).then(({ data, response }) => {
+    console.log(`Status Code: ${response.statusCode}`)
+    console.log(data)
+})
+
+
+ 
+// try {
+//   const parser = new Json2csvParser(opts);
+//   const csv = parser.parse(myData);
+//   console.log(csv);
+// } catch (err) {
+//   console.error(err);
+// }
