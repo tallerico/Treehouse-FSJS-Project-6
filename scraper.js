d8c9016fef6d6bb1f3855141c097b147d91970cf
Mike Tallerico <mtallerico1@gmail.com>
2018-06-26 16:44:24 -0400

    setting up package.json and working through scraper

--- scraper.js
+++ scraper.js
@@ -1,1 +1,5 @@
-var scrape = require('website-scraper');
+const scraper = require('scrape-it');
+const fs = require('fs');
+const csvParse = require('json2csv').Parser;
+const folderExist = fs.existsSync('./data');
+
