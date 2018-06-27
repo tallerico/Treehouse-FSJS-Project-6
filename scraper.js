2a295dc390b6561985c8194ad10ba523971a9645
Mike Tallerico <mtallerico1@gmail.com>
2018-06-27 07:44:23 -0400

    functions for building objects with tshirt data and then adding them to an array of tshirts.

--- scraper.js
+++ scraper.js
@@ -1,8 +1,45 @@
 const scrapeIt = require('scrape-it');
+const Json2csvParser = require('json2csv').Parser;
 const fs = require('fs');
 const folderExist = fs.existsSync('./data');
-const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+let fields = [];
+let myShirts = [];
 const opts = { fields };
+const today = new Date();
+
+function Tshirt(title,price, imgURL, url, curTime) {
+    this.title = title;
+    this.price = title;
+    this.imgURL = title;
+    this.title = title;
+    this.title = title;
+}
+
+function scrapePages(pageURL) {
+    scrapeIt(`${pageURL}`, {
+        title:{
+            selector: ".shirt-picture img",
+            attr: "alt"
+            },
+            price: 'h1 .price',
+            imageURL: {
+                selector: ".shirt-picture img",
+                attr: "src"
+            }
+        }
+    ).then(({data,response}) => {
+        const obj = {
+            Title: data.title,
+            Price: data.price,
+            ImageUrl: data.imageURL,
+            URL: pageURL,
+            Time: `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+        }
+        myShirts.push(obj);
+    })
+}
+
+// scrapePages('http://shirts4mike.com/shirt.php?id=102');
 
 scrapeIt('http://shirts4mike.com/shirts.php', {
     tshirts: {
@@ -12,25 +49,20 @@ scrapeIt('http://shirts4mike.com/shirts.php', {
              url: {
                   selector: "a"
                 , attr: "href"
-              },
-              img: {
-                  selector: "img",
-                  attr: "src"
               }
           }
       }
     
 }).then(({ data, response }) => {
-    console.log(`Status Code: ${response.statusCode}`)
-    console.log(data)
+    const myData = data.tshirts;
+    for (let tshirt of myData) {
+        // console.log(tshirt.url);
+        scrapePages(`http://shirts4mike.com/${tshirt.url}`);
+    }
+    
+    console.log(`Status Code: ${response.statusCode}`);
 })
 
+setTimeout(() => console.log(myShirts), 5000);
 
  
-// try {
-//   const parser = new Json2csvParser(opts);
-//   const csv = parser.parse(myData);
-//   console.log(csv);
-// } catch (err) {
-//   console.error(err);
-// }
